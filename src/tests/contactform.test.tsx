475a8bbfd2b394e38e27e85edbd6e0a8a44eb4df
samriz <samriz@users.noreply.github.com>
2026-01-07 19:44:32 -0600

    Update contactform.test.tsx

--- src/tests/contactform.test.tsx
+++ src/tests/contactform.test.tsx
@@ -4,7 +4,8 @@ import ContactForm from '../components/contactform.tsx';
 //describe-block is the test suite
 describe('ContactForm', () => {
   //it-block (which also can be named test instead of it) is the test case
-  it('renders ContactForm component', () => {
+  test('renders ContactForm component', () => {
+    //Whenever you write a test for a component with React Testing library, you can render the component first and then debug what's visible for RTL's renderer in the test.
     render(<ContactForm/>);
 
     //use RTL's debug function to see rendered output of RTL's render function:
