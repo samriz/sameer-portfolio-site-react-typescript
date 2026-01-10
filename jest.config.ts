6654ef435083cb71d4140d64b1e1dee494a3a962
samriz <samriz@users.noreply.github.com>
2026-01-09 18:18:05 -0600

    Fixed indentation.

--- jest.config.ts
+++ jest.config.ts
@@ -2,15 +2,15 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest', // Use ts-jest for TypeScript
-  testEnvironment: 'jsdom', // Simulate a browser environment
-  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // Point to the setup file
-  // Optional: if you have path aliases in tsconfig.json (e.g., "@/components")
-  /*
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-  },
-  */
+  	preset: 'ts-jest', // Use ts-jest for TypeScript
+  	testEnvironment: 'jsdom', // Simulate a browser environment
+  	setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'], // Point to the setup file
+  	// Optional: if you have path aliases in tsconfig.json (e.g., "@/components")
+  	/*
+  	moduleNameMapper: {
+    	'^@/(.*)$': '<rootDir>/src/$1',
+  	},
+  	*/
 };
 
 export default config;
