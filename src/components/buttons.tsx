f4baedf5dca0f2e8c57b11f7b6399b01a33e43d4
samriz <samriz@users.noreply.github.com>
2026-01-14 18:00:37 -0600

    Update buttons.tsx

--- src/components/buttons.tsx
+++ src/components/buttons.tsx
@@ -1,3 +1,5 @@
+//import React from 'react';
+
 interface DownloadButtonProps {
     file: any;
     buttonText: string;
@@ -9,3 +11,18 @@ export function DownloadButton({file, buttonText}: DownloadButtonProps)
         <button className={"leftPad roundedCorners"}><a href={file} download>{buttonText}</a></button>
     );
 }
+
+/* export default class DownloadButton extends React.Component<DownloadButtonProps>
+{
+    render()
+    {
+        return (
+            <button type="button" onClick={async () => await this.getResource(this.props.file)}>{this.props.buttonText}</button>
+        );
+    }
+    
+    getResource = async (resource: any) => {
+        const response = await fetch(resource);
+        await response.json();
+    }
+} */
